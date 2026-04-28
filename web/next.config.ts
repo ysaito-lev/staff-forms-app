2ea6507e40adbc80af0e49f65bc17eebbce9fbd9
ysaito-lev <ysaito-lev@users.noreply.github.com>
2026-04-28 16:52:06 +0900

    デプロイ用マニフェストファイル作成

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   images: {
     /** `NEXT_PUBLIC_LEVELA_LOGO_URL` が別ホストのときはここに hostname を追加 */
     remotePatterns: [
